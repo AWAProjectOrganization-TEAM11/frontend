6bbe25d53a9f5fe0da2b3102d97b72bced8262c4
skemoff <t9kesa00@students.oamk.fi>
2021-12-07 12:09:59 +0200

    Shopping cart

---
+++ Shoppingcart/src/data.js
@@ -0,0 +1,20 @@
+const data = {
+    products: [
+        {id: '1',
+        name: 'Borgar',
+        price: 10,
+        image:'https://i.media.fi/incoming/ujb6vi/7260078.jpg/alternates/FREE_1440/7260078.jpg'
+        },
+        {id: '2',
+        name: 'Kebab',
+        price: 11,
+        image:'https://upload.wikimedia.org/wikipedia/commons/a/ad/D%C3%B6ner_kebab_slicing.jpg'
+        },
+        {id: '3',
+        name: 'Sorse',
+        price: 3,
+        image:'https://i.ytimg.com/vi/HIo9VqcOpps/maxresdefault.jpg'
+        },
+    ]
+};
+export default data;
